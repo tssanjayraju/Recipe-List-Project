6ebacf827ef68d9d9e3a7355954920bfb019bce2
T S Sanjay Raju <tssanjayraju@gmail.com>
2017-09-28 19:55:18 +1000

    shopping list added

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients: Ingredient[] = [
+  new Ingredient('Chicken', 2),
+  new Ingredient('Tomatoes', 7),
+  new Ingredient('Yoghurt', 1)
+];
   constructor() { }
 
   ngOnInit() {
